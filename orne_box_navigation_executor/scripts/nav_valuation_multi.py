d70cf694ffc8b7d2ebd3f3e086a18bda3fa0d7dd
masakifujiwara1 <fmasaki2020@gmail.com>
2024-11-21 18:16:07 +0900

    Update

--- orne_box_navigation_executor/scripts/nav_valuation_multi.py
+++ orne_box_navigation_executor/scripts/nav_valuation_multi.py
@@ -8,8 +8,10 @@ from nav_msgs.msg import Odometry
 import numpy as np
 import time
 
-min_dist = 1e4
-min_ttc = 1e4
+min_dist1 = 1e4
+min_dist2 = 1e4
+min_ttc1 = 1e4
+min_ttc2 = 1e4
 start_time = None
 navigation_duration = 0.0
 navigation_started = False
@@ -17,11 +19,12 @@ navigation_finished = False
 total_distance = 0.0
 prev_pos = None
 prev_time = None
-sum_dist = 0.0
+sum_dist1 = 0.0
+sum_dist2 = 0.0
 dist_count = 0
 
 def callback_dist(data):
-    global min_dist, min_ttc, navigation_finished, sum_dist, dist_count, prev_time
+    global min_dist1, min_dist2, min_ttc1, min_ttc2, navigation_finished, sum_dist1, sum_dist2, dist_count, prev_time
     try:
         if navigation_finished:
             return
@@ -40,23 +43,34 @@ def callback_dist(data):
         actor1_position = calc_pose(poses, actor1_index)
         actor2_position = calc_pose(poses, actor2_index)
 
+        orne_box_velocity = np.array([twists[orne_box_index].linear.x, twists[orne_box_index].linear.y])
+        actor1_velocity = np.array([twists[actor1_index].linear.x, twists[actor1_index].linear.y])
+        actor2_velocity = np.array([twists[actor2_index].linear.x, twists[actor2_index].linear.y])
+
         # ユークリッド距離を計算
         distance1 = np.linalg.norm(orne_box_position - actor1_position)
         distance2 = np.linalg.norm(orne_box_position - actor2_position)
-        sum_dist += distance1 + distance2
+        sum_dist1 += distance1
+        sum_dist2 += distance2
         dist_count += 1
 
-        min_dist = min(min_dist, distance1)
-        min_dist = min(min_dist, distance2)
+        min_dist1 = min(min_dist1, distance1)
+        min_dist2 = min(min_dist2, distance2)
 
-        # # 相対速度を計算
-        # relative_velocity = orne_box_velocity - actor2_velocity
+        # 相対速度を計算
+        relative_velocity1 = orne_box_velocity - actor1_velocity
+        relative_velocity2 = orne_box_velocity - actor2_velocity
 
-        # # TTCを計算
-        # if np.linalg.norm(relative_velocity) > 1e-6:  # ゼロ除算回避
-        #     ttc = distance / np.linalg.norm(relative_velocity)
-        #     if ttc > 0: # TTCが正の値の場合のみ更新
-        #         min_ttc = min(min_ttc, ttc)
+        # TTCを計算
+        if np.linalg.norm(relative_velocity1) > 1e-6:  # ゼロ除算回避
+            ttc = distance1 / np.linalg.norm(relative_velocity1)
+            if ttc > 0: # TTCが正の値の場合のみ更新
+                min_ttc1 = min(min_ttc1, ttc)
+
+        if np.linalg.norm(relative_velocity2) > 1e-6:  # ゼロ除算回避
+            ttc = distance2 / np.linalg.norm(relative_velocity2)
+            if ttc > 0: # TTCが正の値の場合のみ更新
+                min_ttc2 = min(min_ttc2, ttc)
 
     except ValueError:
         # orne_boxまたはactor2が見つからない場合の処理
@@ -76,14 +90,15 @@ def start_navigation(data):
         rospy.loginfo("Navigation started.")
 
 def finish_navigation(data):
-    global start_time, navigation_duration, navigation_finished, total_distance, sum_dist, dist_count
+    global start_time, navigation_duration, navigation_finished, total_distance, sum_dist1, sum_dist2, dist_count
     if start_time is not None and data.data and not navigation_finished:
         navigation_duration = time.time() - start_time
         navigation_finished = True
         avg_speed = total_distance / navigation_duration if navigation_duration > 0 else 0.0
-        avg_dist = sum_dist / dist_count if dist_count > 0 else 0.0
+        avg_dist1 = sum_dist1 / dist_count if dist_count > 0 else 0.0
+        avg_dist2 = sum_dist2 / dist_count if dist_count > 0 else 0.0
         rospy.loginfo("Navigation finished. Duration: %.3f seconds", navigation_duration)
-        rospy.loginfo(f'TIME: {navigation_duration:.3f} sec, DISTANCE: {total_distance:.3f} m, MIN_DIST: {min_dist:.3f} m, AVG_SPEED: {avg_speed:.3f} m/s, AVG_DIST: {avg_dist:.3f} m, MIN_TTC: {min_ttc:.3f} s')
+        rospy.loginfo(f'TIME: {navigation_duration:.3f} sec, DISTANCE: {total_distance:.3f} m, MIN_DIST_1: {min_dist1:.3f} m, , MIN_DIST_2: {min_dist2:.3f} m, AVG_SPEED: {avg_speed:.3f} m/s, AVG_DIST_1: {avg_dist1:.3f} m, AVG_DIST_2: {avg_dist2:.3f} m, MIN_TTC_1: {min_ttc1:.3f} s, MIN_TTC_2: {min_ttc2:.3f} s')
 
 def odom_callback(data):
     global total_distance, prev_pos, navigation_started, navigation_finished, prev_time
